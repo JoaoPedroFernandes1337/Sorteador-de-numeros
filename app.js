b8429d3a5a78c04bd7077e9047ce5ad7bd42e9df
Joao Pedro Fernandes <joaopf0303@gmail.com>
2026-01-14 09:07:37 -0300

    Adicionando proteção para o sorteio

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+
+    if (de >= ate) {
+    resultado.innerHTML = `<label class="texto__paragrafo">Erro! O campo "Do número" deve ser inferior ao campo "Até o número". </label>`;
+    return;
+  }
     
     let sorteados = [];
     let numero;
 
     for (let i = 0; quantidade > i; i++){
-        numero = obterNumeroAleatorio(de, ate);
-        while (sorteados.includes(numero)){
+        if (quantidade > ((ate - de) + 1)){
+            let resultado = document.getElementById('resultado');
+            resultado.innerHTML = `<label class="texto__paragrafo">Erro! Quantidade requerida de números sorteados é maior que as possibilidades possiveis! </label>`;
+        }else {
             numero = obterNumeroAleatorio(de, ate);
+            while (sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+            }
+            sorteados.push(numero);
+            let resultado = document.getElementById('resultado');
+            resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+            alterarStatusBotao();
         }
-
-        sorteados.push(numero);
     }
-
-     let resultado = document.getElementById('resultado');
-     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
-     alterarStatusBotao();
 }
-
 function obterNumeroAleatorio(min, max){
         return Math.floor(Math.random() * (max - min +1)) +min;
 }
